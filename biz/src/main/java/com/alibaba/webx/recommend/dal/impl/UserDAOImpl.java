ff62c31c0b34cb5e814e802c865113bff07e1e51
ywf4026 <ywf4026@163.com>
2013-12-01 11:11:54 +0800

    local test

--- biz/src/main/java/com/alibaba/webx/recommend/dal/impl/UserDAOImpl.java
+++ biz/src/main/java/com/alibaba/webx/recommend/dal/impl/UserDAOImpl.java
@@ -5,7 +5,7 @@ import com.alibaba.webx.recommend.dal.UserDAO;
 public class UserDAOImpl implements UserDAO {
     @Override
     public void a() {
-        //To change body of implemented methods use File | Settings | File Templates.
+          //To change body of implemented methods use File | Settings | File Templates.
     }
 
 }
